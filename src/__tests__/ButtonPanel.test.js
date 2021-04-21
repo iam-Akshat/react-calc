0f58bea6edb4896ac96d70c7c1da069e7fd63089
Akshat <masterakshata@gmail.com>
2021-04-22 02:01:53 +0530

    add tests for ButtonPanel component

---
+++ src/__tests__/ButtonPanel.test.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import ButtonPanel from '../components/ButtonPanel';
+
+describe('ButtonPanel', () => {
+  test('renders all buttons', () => {
+    const { container } = render(<ButtonPanel clickHandler={n => n} />);
+    const component = container.querySelector('.buttons_container');
+    expect(component.textContent).toBe('AC-/+%÷789X456-123+0.=');
+  });
+});
