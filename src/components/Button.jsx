e5d5cce32a999fbb153d215e8f4217e97cc1fd9c
Akshat <masterakshata@gmail.com>
2021-04-21 18:01:44 +0530

    add styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import PropTypes from 'prop-types';
 
-const Button = ({ name, clickHandler }) => (
-  <button type="button" className="button" onClick={() => { clickHandler(name); }}>
+const Button = ({ name, clickHandler, specialClassNames }) => (
+  <button type="button" className={`button ${specialClassNames}`} onClick={() => { clickHandler(name); }}>
     {name}
   </button>
 );
