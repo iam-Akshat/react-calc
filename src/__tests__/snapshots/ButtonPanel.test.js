4301c22d2defe54ceeb02681affccc6739652fb4
Akshat <masterakshata@gmail.com>
2021-04-22 13:33:30 +0530

    add snapshot tests for Button and ButtonPanel

---
+++ src/__tests__/snapshots/ButtonPanel.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ButtonPanel from '../../components/ButtonPanel';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ButtonPanel />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
